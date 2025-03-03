549be91bc485afde82ba912098d936d27cfee96a
LukasBeke <lbekevicius@gmail.com>
2025-03-03 11:25:12 +0200

    2025

--- scripts.js
+++ scripts.js
@@ -1,12 +1,14 @@
-document.getElementById("myP1").style.color = "red";
-myP1.style.backgroundColor = "black";
+document.getElementById("myP1").style.color = "black";
+myP1.style.backgroundColor = "red";
 
-document.getElementById("myP2").style.color = "black";
+document.getElementById("myP2").style.color = "white";
+myP2.style.backgroundColor = "black";
 
-document.getElementById("myP3").style.color = "red";
-myP3.style.backgroundColor = "black";
+document.getElementById("myP3").style.color = "black";
+myP3.style.backgroundColor = "red";
 
-document.getElementById("myP4").style.color = "green";
+document.getElementById("myP4").style.color = "white";
+myP4.style.backgroundColor = "black";
 
-document.getElementById("myP5").style.color = "red";
-myP5.style.backgroundColor = "black";
+document.getElementById("myP5").style.color = "white";
+myP5.style.backgroundColor = "green";
